e8c252a96cb34181d966859a6a36b21c94a7152f
Carlos Augusto de Sousa <carlos_sousamn@yahoo.com.br>
2020-01-10 07:22:10 -0300

    Indo para a implatação no Heroku!

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python_web
 Este é um projeto de python do Livro  Curso intensivo de Python
+no qual vou ultilizar Jango para montar uma pequena Aplicação web para simular um Site!
---
+++ learning_log/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/carlim/Livros_programacao/Curso_Intensivo_De_Python/Projetos/Python_web/learning_log/ll_env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
